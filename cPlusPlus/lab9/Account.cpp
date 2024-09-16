3e3cb65431a87184bb6c6d2d7fd4f4b2a374226f
RushilShandilya <rushilcujoh@gmail.com>
2024-09-16 13:22:00 +0530

    Commit

--- cPlusPlus/lab9/Account.cpp
+++ cPlusPlus/lab9/Account.cpp
@@ -3,138 +3,63 @@ using namespace std;
 
 class Account{
 	private:
-		int accountID;
+		int accountHolderID;
 		string accountHolderName;
-		double balance;
+		double accountHolderBalance;
 	public:
-		static int countAccount;
+		static int countObject;
 		Account(){}
-		Account(int accountID,string accountHolderName,double balance):accountID(accountID),accountHolderName(accountHolderName),balance(balance){countAccount++;}
-
-		int getAccountID(){
-			return this->accountID;
-		}
-		void setBalance(int balance){
-			this->balance = balance;
-		}
-		int getBalance(){
-			return this->balance;
-		}
-		void AcceptData(){
-		}
-		void DisplayData(){
-			cout<<"The address of the invoked object : "<<this<<endl;
-			cout<<"Account holder name : "<<this->accountHolderName<<endl;
-			cout<<"Account Balance : "<<this->balance<<endl;
-		}
-		void WithdrawAmount(double amountToWithdraw,Account* desiredAccount){
-			int amountRemaining = desiredAccount->getBalance() - amountToWithdraw;
-			if(amountRemaining>0){
-				cout<<"New balance after withdrawl : "<<amountRemaining<<endl;
-				desiredAccount->setBalance(amountRemaining);
-				cout<<"Goodbye"<<endl;
-			}
-			else cout<<"Not sufficient Balance"<<endl;
-				
-		}
-		void DepositAmount(double amountToDeposit,Account* desiredAccount){
-			
-			int newAmount = desiredAccount->getBalance()+amountToDeposit;
-		       	desiredAccount->setBalance(newAmount);
-			cout<<"New balance after deposit : "<<desiredAccount->balance<<endl;
-			return;	
+		Account(int accountHolderID,string accountHolderName,double accountHolderBalance):
+			accountHolderID(accountHolderID),accountHolderName(accountHolderName),accountHolderBalance(accountHolderBalance){++countObject;}
+		void acceptData(){}
+		void displayData(){
+			cout<<"AccountHolderID : "<<this->accountHolderID<<endl;
+			cout<<"Name : "<<this->accountHolderName<<endl;
+			cout<<"Balance : "<<this->accountHolderBalance<<endl;
 		}
 };
 
-void createNewAccount(Account** accountList){	
-	char ch;
+void createAccount(Account** listOfPointerForAccounts){
+	char toContinue;
 	do{
 		string getName;
 		double balance;
-		int accountID = Account::countAccount+1;
 
-		cout<<"Enter the account holder name : "<<endl;
+		cout<<"Beginning Account Creation .........."<<endl;
+		cout<<"Enter name : "<<endl;
 		getline(cin,getName);
-		cout<<"Enter Initial Money (0 by default): "<<endl;
+		cout<<"Enter Balance : "<<endl;
 		cin>>balance;
-		
-
-		Account* account = addressof(Account(accountID,getName,balance));
-		accountList[Account::countAccount] = account;
-
 		cin.get();
-		cout<<"Do you want to add more accounts ? Type y/n "<<endl;
-		cin>>ch;
-		cin.get();
-	}while(ch!='n'&&ch!='N');
-}
-Account* findAccount(int accountID,int numberOfValidAccounts,Account** listOfAccount){
-	Account garbage = Account(-1,"No new account was found",0.0);
 
-	for(int i = 0 ; i<numberOfValidAccounts ; ++i){
-		if(listOfAccount[i]->getAccountID()==accountID)
-			return &listOfAccount[i];
-	}
-	return &garbage;
+		listOfPointerForAccounts[Account::countObject]=new Account(Account::countObject+1,getName,balance);
+		cout<<"Account creation successfull"<<endl;
+		cout<<"Do you want to continue ? Yes/No (y/n)"<<endl;
+		cin>>toContinue;
+		cin.get();	
+	}while(toContinue!='n' && toContinue!='N');
 }
-int Account::countAccount=0;
+
+int Account::countObject=0;
+
 int main(){
-	
-	Account* accountList[10000];
-	cout<<"Welcome to Rushil Bank Pvt. Limited"<<endl;
+	Account* listOfPointerForAccounts[10000];
 	char toExitTheMenu;
 	do{
-		int enterMenuOption;
-		int accountID;
-		Account* account;
-		cout<<"Please choose b/w the given options(Press 1,2,3,4) : "<<endl;
-		cout<<"1. Create a new bank account "<<endl;
-		cout<<"2. Display data of a bank account "<<endl;
-		cout<<"3. Withdraw money to account "<<endl;
-		cout<<"4. Deposit money from account "<<endl;
-		cin>>enterMenuOption;	
-		cin.get();	
-		switch(enterMenuOption){
-			case 1:
-				createNewAccount(accountList);
-				cout<<"New Account(s) created successfully"<<endl;
-				break;
-			case 2:
-				cout<<"Enter AccountID : "<<endl;
-				cin>>accountID;
-				account = findAccount(accountID,Account::countAccount,accountList);
-				account->DisplayData();
-				break;
-			case 3:
-				int withdrawAmount;
-				cout<<"Enter AccountID : "<<endl;
-				cin>>accountID;
-				account = findAccount(accountID,Account::countAccount,accountList);
-				if(account->getAccountID()>0){
-					cout<<"Enter the money to withdraw : "<<endl;
-					cin>>withdrawAmount;
-					account->WithdrawAmount(withdrawAmount,account);
-				}else cout<<"Account doesn't exist"<<endl;
-
-				break;
-
-			case 4:
-				int depositAmount;
-				cout<<"Enter AccountID : "<<endl;
-				cin>>accountID;
-				account = findAccount(accountID,Account::countAccount,accountList);
-				if(account->getAccountID()>0){
-					cout<<"Enter the money to deposit : "<<endl;
-					cin>>depositAmount;
-					account->DepositAmount(depositAmount,account);
-				}else cout<<"Account doesn't exist"<<endl;
-				break;
-			defeault:
-				cout<<"Waiting for correct operation"<<endl;
-		}
-		cout<<"Do you want to go back to menu? Y/N : "<<endl;
+		createAccount(listOfPointerForAccounts);
+		cout<<"Do you want to exit program : Yes/No(y/n)"<<endl;
 		cin>>toExitTheMenu;
-	}while(toExitTheMenu!='n' && toExitTheMenu!='N');
-
+	}while(toExitTheMenu!='y'&& toExitTheMenu!='Y');
+	
+	cout<<"Total Object Created :"<<Account::countObject<<endl;
+	
+	for(int i = 0; i<=Account::countObject ; ++i){
+		if(listOfPointerForAccounts[i]==nullptr){
+			delete listOfPointerForAccounts[0];
+			cout<<"It's null pointer "<<endl;
+		}
+		else listOfPointerForAccounts[i]->displayData();
+		delete listOfPointerForAccounts[i];
+	}
 	return 0;
 }
